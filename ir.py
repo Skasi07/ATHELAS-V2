9393ddd2d6699e4535d4a9abe1ee04cd061601f6
sreevaths kasi <ksreevaths@gmail.com>
2017-10-26 16:45:12 -0700

    Update ir.py

--- ir.py
+++ ir.py
@@ -1,10 +1,10 @@
 import time
 import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(12, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 while True:
 
-	print('VALUE', GPIO.input(18))
+	print('VALUE', GPIO.input(12))
 	time.sleep(0.1)
 
